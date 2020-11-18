97f10ddb3777d30986396a493fabee92c3a87752
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2020-11-18 16:21:32 +0400

    added express routes

---
+++ index.js
@@ -0,0 +1,20 @@
+// Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const router = require('./router');
+
+const app = express();
+
+// App setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+//Server Setup
+
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening to port:', port);
