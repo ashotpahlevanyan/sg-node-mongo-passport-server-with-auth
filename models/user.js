488eba8cfafb622f154df0b5b43166b1ca03bee1
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2020-11-18 16:32:04 +0400

    added user schema

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// define our model
+const userSchema = new Schema({
+  email: { type: String, unique: true, lowercase: true },
+  password: { type: String }
+});
+
+// create the model class
+const ModelClass = mongoose.model('user', userSchema);
+
+
+// export the model
+module.exports = ModelClass;
+
