8818719e592ba5ba1d0c037036dc3f30651f81f0
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2020-11-19 13:21:23 +0400

    added user creation

---
+++ controllers/authentication.js
@@ -0,0 +1,41 @@
+const User = require('../models/user');
+
+
+exports.signup = function (req, res, next) {
+ // see if a user with a given email exists
+
+  const {email, password} = req.body;
+
+  if(!email || !password) {
+    return res.status(422).send({ error: 'You must provide email and password' });
+  }
+  User.findOne({ email: email}, (err, existingUser) => {
+    if(err) {
+      return next(err);
+    }
+
+    // if a user with email exists, return an error
+
+    if(existingUser) {
+      res.status(422).send({error: 'Email is in use'});
+    }
+
+
+    // if user with an email doesn't exist,
+    // create a user record
+    const user = new User({ email: email, password: password });
+    user.save((err) => {
+      if(err) {
+        return next(err);
+      }
+
+      res.json({success: true});
+    });
+
+
+
+    // respond to request indicating the user was created
+
+  });
+
+};
--- index.js
+++ index.js
@@ -4,13 +4,25 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router = require('./router');
-
+const mongoose = require('mongoose');
 const app = express();
 
+
+// DB Setup
+
+mongoose.connect('mongodb://localhost:auth/auth', {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+  console.log('connected');
+}).catch(e => {
+  console.log('error: ', e);
+});
+
+
 // App setup
+
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
+
 
 //Server Setup
 
